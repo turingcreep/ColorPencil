5a60eb5c33b217f2eb7b6e14630c6a9a17f2a87b
Shankar Sengupta <shankar.sengupta.dev@gmail.com>
2015-12-30 16:33:45 -0800

    Scaffolding for lexer, parser and evaluator

--- src/colorpencil.js
+++ src/colorpencil.js
@@ -1,6 +1,12 @@
+import Lexer from 'lexer';
+import Parser from 'parser';
+import Evaluator from 'evaluator';
 export default class Colorpencil{
-	constructor(){
-
+	constructor(configFile="color.conf.js"){
+		this.configFile = configFile;	
+		this.lexer = new Lexer();
+		this.parser = new Parser();
+		this.evaluator = new Evaluator();
 	}
 	test(){
 		console.log("testing ::Colorpencil");
