dfde7bffcd00b0d7415721653c2536ce0b3f4e42
Shankar Sengupta <shankar.sengupta.dev@gmail.com>
2015-12-30 16:43:51 -0800

    Adding test spec file; Tests are jasmine;

---
+++ tests/colorpencil.spec.js
@@ -0,0 +1,12 @@
+describe("colorpencil",function(){
+	it("returns an obect when constructed",function(){
+		var	cpencil = new Colorpencil();
+		expect(cpencil).toBeNotNull();
+	});
+	it("instance can take in an input string and generate a classed html output",function(){
+		var 	cpencil = new Colorpencil(),
+			fragment = '';
+		fragment = cpencil.write('red');
+		expect(fragment).toBe('<span class="text text-red">red</span>');
+	});
+});
